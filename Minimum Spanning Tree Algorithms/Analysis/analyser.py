24abee3e202bdb879f23ce22a433cb299c3ff95c
Raiyan <raiyan.c15@gmail.com>
2023-03-06 09:50:06 +0000

    back at labs

--- Minimum Spanning Tree Algorithms/Analysis/analyser.py
+++ Minimum Spanning Tree Algorithms/Analysis/analyser.py
@@ -7,12 +7,12 @@ import csv
 
 
 class Data():
-  def __init__(self, entry) -> None:
+  def __init__(self) -> None:
     self.vertex_to_entry_dict = defaultdict(list) # vertex: [entry1, entry3, entry3]
     self.vertex_to_avgs = {} # vertex: avg_time
 
   def add_entry(self, entry):
-    self.vertex_to_entry_dict[entry.vertex].append(entry)
+    self.vertex_to_entry_dict[entry.vertices].append(entry)
     # self.update_average(entry)
 
   def calculate_average(self): 
@@ -26,6 +26,11 @@ class Data():
       avg = tot / size
       self.vertex_to_avgs[k] = avg
 
+  def plot(self):
+    x, y = zip(*sorted(data.vertex_to_avgs.items()))
+    plt.plot(x, y)
+    plt.show()
+
 
 class Entry():
   def __init__(self, vertices, edges, st, et) -> None:
@@ -33,28 +38,27 @@ class Entry():
     self.edges = edges
     self.st = st
     self.et = et
-
   
+  def __repr__(self) -> str:
+    return f"{self.vertices},{self.edges},{self.st},{self.et}\n"
 
-def read(path):
-  with open(path) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-        else:
-            
-    print(f'Processed {line_count} lines.')
-
-
-
+      
 if __name__ == "__main__":
   args = argparse.ArgumentParser()
-  args.add_argument('-p', '--path', required=True, help="path of csv")
+  args.add_argument('-p', '--path', nargs='+', required=True, help="Path of CSV files to plot")
+
   args = args.parse_args()
 
+  data = Data()
+  with open(args.path[0]) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
+    for row in csv_reader:
+      vertices, edges, st, et = row
+      e = Entry(int(vertices), int(edges), int(st), int(et))
+      data.add_entry(e)
 
-  read(args.path)
+  data.calculate_average()
+  data.plot()
 
 
